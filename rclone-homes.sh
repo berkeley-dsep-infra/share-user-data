c05d06e8f2d708c83e3fb5d3595e830ddcbad604
ryanlovett <rylo@berkeley.edu>
2019-01-24 17:21:42 -0800

    Make and upload an md5sum for each tarball.
    Helps with verifying work. (i.e. incompletes)

--- rclone-homes.sh
+++ rclone-homes.sh
@@ -18,23 +18,27 @@ for d in ${dirlist} ; do
     if [ ! -d $d ]; then continue ; fi
     echo $d
     f=${d}.tar.gz
-    exists=$(rclone lsf ${rclone_target}:"${folder}"/${d}.tar.gz 2>/dev/null)
-    if [ ! -z "${exists}" ]; then
+    exists=$(rclone lsf ${rclone_target}:"${folder}"/${f} 2>/dev/null)
+    exists_md5=$(rclone lsf ${rclone_target}:"${folder}"/${f}.md5sum 2>/dev/null)
+	# we don't do anything if both tarball and md5 exist in the cloud
+    if [ ! -z "${exists}" -a ! -z "${exists_md5}" ]; then
         echo $d already uploaded
         continue
     fi
     if [ ! -f $f ]; then
         echo creating $f
         tar -c -z --exclude=.git --exclude=.gz --exclude=.zip --exclude=.nii -f $f ${d}
+		# update md5sum whenever we make the tarball
+		md5sum $f > ${f}.md5sum
     fi
     if [ ! -f $f ]; then
         echo error $d
         continue
     fi
     echo copying $d
-    rclone copy $f ${rclone_target}:"${folder}"/
+    rclone copy $f ${f}.md5sum ${rclone_target}:"${folder}"/
     if [ $? -eq 0 ]; then
-        rm -f $f
+        rm -f $f ${f}.md5sum
     else
         echo error copying $f
     fi
